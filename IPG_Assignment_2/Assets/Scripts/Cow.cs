09e83c0bcf6d62e37c3ea8842e3c89bec0760d6e
Holly Clibbon <vendorofdoom@users.noreply.github.com>
2022-01-10 16:55:22 +0000

    Randomly assign a cow's status every 30 seconds

--- IPG_Assignment_2/Assets/Scripts/Cow.cs
+++ IPG_Assignment_2/Assets/Scripts/Cow.cs
@@ -1,10 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Cow : MonoBehaviour
 {
     public string cowName;
-    public int cowAge;
-    public string cowStatus;
+    public List<string> cowStatusOptions;
+    private string currStatus;
     public bool wearingFlowers;
     public GameObject flowerCrown;
 
@@ -30,12 +32,23 @@ public class Cow : MonoBehaviour
 
     // TODO: Implement ReceiveFlower()
 
+    private void Start()
+    {
+        currStatus = cowStatusOptions[Random.Range(0, cowStatusOptions.Count)];
+        InvokeRepeating("updateStatus", 30f, 30f);
+    }
+
+    private void updateStatus()
+    {
+        currStatus = cowStatusOptions[Random.Range(0, cowStatusOptions.Count)];
+    }
+
     private void OnMouseDown()
     {
         hearts.Emit(1);
 
         textName.text = cowName;
-        textAbout.text = cowName + ", aged " + cowAge.ToString() + ", is feeling " + cowStatus;
+        textAbout.text = currStatus;
         textPurpleFlowers.text = numPurpleFlowers.ToString() + "/" + requiredPurpleFlowers.ToString();
         textPinkFlowers.text = numPinkFlowers.ToString() + "/" + requiredPinkFlowers.ToString();
         textOrangeFlowers.text = numOrangeFlowers.ToString() + "/" + requiredOrangeFlowers.ToString();
@@ -51,6 +64,5 @@ public class Cow : MonoBehaviour
     private void Update()
     {
         flowerCrown.SetActive(wearingFlowers); // TODO: move out of update later
-
     }
 }
