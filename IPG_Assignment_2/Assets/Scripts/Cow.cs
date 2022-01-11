f587ee996340d4528c8eb5e596b7d39aaf27c2f6
Holly Clibbon <vendorofdoom@users.noreply.github.com>
2022-01-11 15:47:37 +0000

    Combine CowController script into Cow script, remove FlowerButtons script as unused

--- IPG_Assignment_2/Assets/Scripts/Cow.cs
+++ IPG_Assignment_2/Assets/Scripts/Cow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Cow : MonoBehaviour
 {
@@ -11,17 +12,55 @@ public class Cow : MonoBehaviour
 
     [Header("UI")]
     public CowUI cowUI;
-    public ParticleSystem hearts;
+    [SerializeField]
+    private ParticleSystem hearts;
 
     [Header("Flowers")]
     public FlowersCollection flowerCollection;
-    public GameObject flowerCrown;
+    [SerializeField]
+    private GameObject flowerCrown;
 
-    private void Start()
+    [Header("NavMesh")]
+    [SerializeField]
+    private NavMeshAgent navMeshAgent;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private CowBehaviour behaviour;
+
+    [Header("Animation")]
+    [SerializeField]
+    private Animator animator;
+
+    private enum CowBehaviour
     {
+        Wander,
+        Follow
+    }
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
         currStatus = cowStatusOptions[Random.Range(0, cowStatusOptions.Count)];
         InvokeRepeating("updateStatus", 30f, 30f);
     }
+
+    private void Update()
+    {
+        switch (behaviour)
+        {
+            case CowBehaviour.Wander:
+                //navMeshAgent.isStopped = true;
+                break;
+            case CowBehaviour.Follow:
+                navMeshAgent.SetDestination(target.position);
+                break;
+        }
+
+        animator.SetBool("isWalking", navMeshAgent.velocity != Vector3.zero);
+    }
+
 
     private void updateStatus()
     {
